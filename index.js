42c752c245aac964c442d9329f0f1c1cfa7d7537
MuhammedK <muhammedk@la.com.tr>
2022-02-25 23:29:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+
+const port = 41;
+
+var result = [
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+  "maşallah",
+];
+
+app.get("/", (req, res) => res.send(result));
+
+app.listen(process.env.PORT || port, () =>
+  console.log(`41 masallah app listening on port ${port}!`)
+);
